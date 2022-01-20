fb85e4a680d96b8bd8dce466a33a14a0b0c11e2c
movinmoe <moritz.scholtyssek@gmail.com>
2022-01-20 14:39:17 +0000

    init

---
+++ blocks/lazyblock-headvisual/block.php
@@ -0,0 +1,7 @@
+<div id="<?php echo $attributes['headvisual-id']; ?>" class="headvisual-headline">
+<?php if ( $attributes['headvisual-headline'] ) : ?>
+    <h1><?php echo $attributes['headvisual-headline']; ?></h1>
+<?php else: ?>
+    <p>The value is False</p>
+<?php endif; ?>
+</div>
---
+++ functions.php
@@ -0,0 +1,88 @@
+<?php
+
+// initalizing
+add_action('wp_enqueue_scripts', 'bootscore_child_enqueue_styles');
+function bootscore_child_enqueue_styles() {
+
+  // child theme styles
+  wp_enqueue_style('parent-style', get_theme_file_uri() . '/style.css');
+
+  // child theme js
+  wp_enqueue_script( 'jquery' );
+  wp_enqueue_script('bootscore-script', get_theme_file_uri() . '/dist/js/theme.min.js', array('jquery'), '1.0', true );
+  wp_enqueue_script('custom-js', get_theme_file_uri() . '/dist/js/custom.min.js', array('jquery'), '1.0', true );
+
+}
+
+// login form styles
+function custom_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/dist/css/login.min.css' );
+}
+add_action( 'login_enqueue_scripts', 'custom_login_stylesheet' );
+
+add_filter( 'login_headerurl', 'my_custom_login_url' );
+function my_custom_login_url($url) {
+    return 'http://else.media';
+}
+
+// backend admin color scheme
+function elsemedia_admin_color_schemes() {
+
+  $theme_dir = get_stylesheet_directory_uri();
+
+  // default
+  wp_admin_css_color( 'theme', ( 'Dynamic (Theme-based)' ),
+    $theme_dir . '/dist/css/scheme-theme.min.css',
+    array( '#23282d', '#fff')
+  );
+
+  // else.media
+  wp_admin_css_color( 'elsemedia', ( 'elsemedia' ),
+    $theme_dir . '/dist/css/scheme-elsemedia.min.css',
+    array( '#23282d', '#fff', '#d54e21' , '#657a84')
+  );
+
+}
+
+add_action('admin_init', 'elsemedia_admin_color_schemes');
+
+// backend default avatar
+add_filter( 'avatar_defaults', 'mytheme_default_avatar' );
+function mytheme_default_avatar( $avatar_defaults )
+{
+    $avatar = get_option('avatar_default');
+
+    $new_avatar_url = get_stylesheet_directory_uri() . '/assets/images/admin.png';
+
+    if( $avatar != $new_avatar_url )
+    {
+        update_option( 'avatar_default', $new_avatar_url );
+    }
+
+    $avatar_defaults[ $new_avatar_url ] = 'else.media Avatar';
+    return $avatar_defaults;
+}
+
+// backend default favicon
+function add_site_favicon() {
+    echo '<link rel="shortcut icon"
+href="' . get_stylesheet_directory_uri() . '/assets/favicons/favicon.ico" />';
+}
+
+add_action('login_head', 'add_site_favicon');
+add_action('admin_head', 'add_site_favicon');
+
+// include plugins
+
+// define path and URL to the LZB plugin.
+define( 'MY_LZB_PATH', get_stylesheet_directory() . '/inc/lzb/' );
+define( 'MY_LZB_URL', get_stylesheet_directory_uri() . '/inc/lzb/' );
+
+// include the LZB plugin.
+require_once MY_LZB_PATH . 'lazy-blocks.php';
+
+// customize the url setting to fix incorrect asset URLs.
+add_filter( 'lzb/plugin_url', 'my_lzb_url' );
+function my_lzb_url( $url ) {
+    return MY_LZB_URL;
+}
