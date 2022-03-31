b5b1542e59f4145b52517d07bbdc6e1a7fe35400
movinmoe <moritz.scholtyssek@gmail.com>
2022-03-31 20:37:30 +0200

    optimizations
    - added imagesload.js for masonry
    - added plugin dir for purge.css

--- src/js/_custom.js
+++ src/js/_custom.js
@@ -77,73 +77,78 @@ jQuery(function ($) {
     var rellax = new Rellax('.rellax');
 
     // init isotope
-    var $container = $('.row-masonry').isotope({
-      // options
-      itemSelector: '.masonry-item',
-      transitionDuration: '0.5s',
-      masonry: {
+    $('.row-masonry').imagesLoaded( function() {
+
+      var $container = $('.row-masonry').isotope({
+        // options
+        itemSelector: '.masonry-item',
+        transitionDuration: '0.5s',
+        masonry: {
         columnWidth: '.masonry-item'
+        }
+      });
+
+      // isotope load more
+      var initShow = 5; //number of items loaded on init & onclick load more button
+      var counter = 3; //counter for load more button
+      var iso = $container.data('isotope'); // get Isotope instance
+
+      loadMore(initShow); //execute function onload
+
+      function loadMore(toShow) {
+        $container.find(".hidden").removeClass("hidden");
+
+        var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
+          return item.element;
+        });
+        $(hiddenElems).addClass('hidden');
+        $container.isotope('layout');
+
+        //when no more to load, hide show more button
+        if (hiddenElems.length == 0) {
+          jQuery("#load-more").hide();
+        } else {
+          jQuery("#load-more").show();
+        };
+
       }
-    });
 
-    // isotope load more
-    var initShow = 5; //number of items loaded on init & onclick load more button
-    var counter = 3; //counter for load more button
-    var iso = $container.data('isotope'); // get Isotope instance
+      //append load more button
+      $container.after('<div class="text-center mt-5 position-relative"><button id="load-more" class="service-listing-button service-listing-button-back btn-default px-4 text-center position-relative">Weitere Bilder</button></div>');
 
-    loadMore(initShow); //execute function onload
+      //when load more button clicked
+      $("#load-more").click(function() {
+        if ($('#filters').data('clicked')) {
+          //when filter button clicked, set initial value for counter
+          counter = initShow;
+          $('#filters').data('clicked', false);
+        } else {
+          counter = counter;
+        };
 
-    function loadMore(toShow) {
-      $container.find(".hidden").removeClass("hidden");
+        counter = counter + initShow;
 
-      var hiddenElems = iso.filteredItems.slice(toShow, iso.filteredItems.length).map(function(item) {
-        return item.element;
+        loadMore(counter);
       });
-      $(hiddenElems).addClass('hidden');
-      $container.isotope('layout');
 
-      //when no more to load, hide show more button
-      if (hiddenElems.length == 0) {
-        jQuery("#load-more").hide();
-      } else {
-        jQuery("#load-more").show();
+      // mobile text limits
+      if (window.innerWidth < 768){
+        $(".service-listing-button-home span").text(function(index, currentText) {
+          if(currentText.length > 7){
+              return currentText.substr(0, 8)+"..";
+          }
+        });
+      };
+      if (window.innerWidth < 1024){
+        $(".wp-block-table td:first-child").text(function(index, currentText) {
+          if(currentText.length > 3){
+              return currentText.substr(0, 2)+":";
+          }
+        });
       };
 
-    }
 
-    //append load more button
-    $container.after('<div class="text-center mt-5 position-relative"><button id="load-more" class="service-listing-button service-listing-button-back btn-default px-4 text-center position-relative">Weitere Bilder</button></div>');
-
-    //when load more button clicked
-    $("#load-more").click(function() {
-      if ($('#filters').data('clicked')) {
-        //when filter button clicked, set initial value for counter
-        counter = initShow;
-        $('#filters').data('clicked', false);
-      } else {
-        counter = counter;
-      };
-
-      counter = counter + initShow;
-
-      loadMore(counter);
     });
-
-    // mobile text limits
-    if (window.innerWidth < 768){
-      $(".service-listing-button-home span").text(function(index, currentText) {
-        if(currentText.length > 7){
-            return currentText.substr(0, 8)+"..";
-        }
-      });
-    };
-    if (window.innerWidth < 1024){
-      $(".wp-block-table td:first-child").text(function(index, currentText) {
-        if(currentText.length > 3){
-            return currentText.substr(0, 2)+":";
-        }
-      });
-    };
 
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 // external libs
 import "./../../lib/rellax/rellax.js" // quiet
-//import "./../../lib/masonry/masonry.js" // quiet
 import "./../../lib/isotope/isotope.js" // quiet
-import "./../../lib/isotope/imagesloaded.js" // quiet
+import "./../../lib/imagesload/imagesload.js" // quiet
 import "./../../lib/lightbox/fslightbox.js" // quiet
 
 // global
